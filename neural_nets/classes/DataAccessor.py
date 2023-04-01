d912d04f5681656c4e0f3cb3e1437cf8b073fbf8
jatin <jatinchowdhury18@gmail.com>
2023-03-31 23:19:04 -0700

    Adding some comments for DataAccessor methods

--- neural_nets/classes/DataAccessor.py
+++ neural_nets/classes/DataAccessor.py
@@ -8,15 +8,25 @@ import pandas as pd
 import chardet
 
 # %%
+# Loads a dataset from a CSV file describing the dataset
 def load_data(data_path):
     with open(data_path, "rb") as f:
         result = chardet.detect(f.read())
     return pd.read_csv(data_path, encoding=result["encoding"])
 
+# Returns a list of all the composers in the dataset
 def get_list_of_composers(data):
     return data["canonical_composer"].unique()
 
+# Filters the dataset by the given list of composer names, and returns the
+# MIDI data from those composers in a single concatenated MIDI stream.
+#
+# Note that if you have a combined composer name, that will still be caught by the filter.
+# For example, if your query includes the composer "Franz List", any entries in the
+# dataset under the composer name "Niccolò Paganini \ Franz List" will also be included
+# in the filtered data.
 def get_data_for_composer(data, composer_names, data_dir='./data'):
+    # Create a list of composers (by index) that match the query
     composer_indexes = []
     composer_list = get_list_of_composers(data)
     for idx, composer in enumerate(composer_list):
@@ -26,12 +36,14 @@ def get_data_for_composer(data, composer_names, data_dir='./data'):
 
     assert len(composer_indexes) > 0, f"Composer name: \"{composer_name}\" not recognized!!"
 
+    # Find all the MIDI files by the given composers
     all_midi_files = []
     for composer_idx in composer_indexes:
         composer_df = data.query(f"canonical_composer == '{composer_list[composer_idx]}'")
         composer_midi_paths = composer_df["midi_filename"].to_list()
         all_midi_files += composer_midi_paths
 
+    # Load the MIDI files and concatenate them into a single MIDI stream
     generator = Generator()
     midi_data = []
     for midi_file in all_midi_files:
@@ -46,6 +58,7 @@ def get_data_for_composer(data, composer_names, data_dir='./data'):
     return np.concatenate(midi_data)
 
 # %%
+# Some test code for validating the above methods
 # main_data = load_data('../data/maestrov3.csv')
 # composers = get_list_of_composers(main_data)
 # midi_data = get_data_for_composer(main_data, 'Nikolai Medtner', '../data')
--- neural_nets/neural_net_jatin.py
+++ neural_nets/neural_net_jatin.py
@@ -9,11 +9,13 @@ import chardet
 from classes.DataAccessor import load_data, get_list_of_composers, get_data_for_composer
 
 # %%
+# Load full dataset and filter data from a set of composers
 main_data = load_data("./data/maestrov3.csv")
 midi_data = get_data_for_composer(main_data, ['George Frideric Handel', 'Nikolai Medtner'])
 print(midi_data.shape)
 
 # %%
+# Construct a basic model
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.GRU(64, input_dim=1, return_sequences=True))
 model.add(tf.keras.layers.GRU(128, return_sequences=True))
@@ -25,6 +27,7 @@ model.compile(loss='mean_squared_error', optimizer=opt)
 model.summary()
 
 # %%
+# Test the model input and output shapes
 inputs = tf.random.normal([1, 10, 1])
 outputs = model(inputs)
 
@@ -33,6 +36,7 @@ print(tf.shape(outputs))
 print(outputs)
 
 # %%
+# Configure MIDI input data for network training
 midi0 = np.reshape(midi_data, (1, len(midi_data), -1))
 time_steps = np.reshape(np.arange(midi0.shape[1]), (1, -1, 1))
 
@@ -43,9 +47,12 @@ print(time_steps.shape)
 
 
 # %%
+#Train the network!
+# Adjust the number of epochs depending on how long you want to train for.
 model.fit(time_steps, midi0, epochs=5)
 
 # %%
+# Print the model output
 print(model(time_steps))
 
 # %%
